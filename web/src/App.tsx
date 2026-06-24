d199b6a6609d340e2449eda6aefb8da8cb4b327f
bcdxn <5067649+bcdxn@users.noreply.github.com>
2026-06-24 12:06:33 -0400

    fix: seo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,18 @@ import LandingPage from "./pages/LandingPage";
 import EditorPage from "./pages/EditorPage";
 import DocsPage from "./pages/DocsPage";
 import SpecPage from "./pages/SpecPage";
+import { useEffect } from "react";
 
 export default function App() {
   const baseUrl = import.meta.env.BASE_URL;
   const basename = baseUrl === "./" ? "/" : baseUrl;
+
+  useEffect(() => {
+    let seo = document.getElementById("seo-content");
+    if (seo) {
+      seo.style.display = "none";
+    }
+  });
 
   return (
     <BrowserRouter basename={basename}>
