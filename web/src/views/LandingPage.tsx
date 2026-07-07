ee6d398cee9470a94dfb8ccb8334ed6053f8f2af
bcdxn <5067649+bcdxn@users.noreply.github.com>
2026-07-07 10:00:16 -0400

    ci: prep for release 1.0.0-alpha.10

--- web/src/views/LandingPage.tsx
+++ web/src/views/LandingPage.tsx
@@ -7,7 +7,7 @@ import SiteFooter from "../components/SiteFooter";
 import React from "react";
 import { useI18n } from "../i18n";
 
-const yamlSampleEn = `opencliVersion: 1.0.0-alpha.9
+const yamlSampleEn = `opencliVersion: 1.0.0-alpha.10
 
 info:
   title: Pleasantries CLI
@@ -30,7 +30,7 @@ commands:
         type: "string"
         default: "english"`;
 
-const yamlSampleCh = `opencliVersion: 1.0.0-alpha.9
+const yamlSampleCh = `opencliVersion: 1.0.0-alpha.10
 
 info:
   title: 寒暄
